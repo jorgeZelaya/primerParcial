9545741e33c293b752046ea130250965e5a738bd
jorgeZelaya <ulloa45@gmail.com>
2016-08-25 23:57:25 -0600

    parcial

---
+++ primerParcial/bootstrap/js/cargarDatos.js
@@ -0,0 +1,20 @@
+
+$(document).on('click','.delete',function(){
+
+			if (confirm('¿Estas seguro de enviar este formulario?')){
+			       $(this).parents("tr").remove();
+			    } 
+
+
+			 else
+			 {
+
+			 }
+
+});
+
+
+
+
+
+  
